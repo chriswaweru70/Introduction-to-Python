5dfdcf3a48146ba1191f2faff623211dba5dd680
Chris Waweru <chriswaweru70@gmail.com>
2022-09-05 19:30:50 -0400

    Printing a file

---
+++ Activity-1.py
@@ -0,0 +1,9 @@
+#My Name is Chris Waweru.
+myName = "Chris Waweru"
+#I am 26 years old.
+myAge = 26
+#Here is my name and age
+print(myName)
+print(myAge)
+
+print("The answer is:", 6 + 7, '!')
---
+++ Activity-2.py
@@ -0,0 +1,5 @@
+#My age is 26
+myAge = 26
+daysLived = (myAge * 365)
+#Days lived
+print(daysLived)
